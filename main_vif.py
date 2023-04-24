rescale plots, plot predicted AIF curves, quality loss WIP, fix 32 rep weighting

main_vif.py
@@ -24,22 +24,24 @@ def inference_mode(args):
     volume_img = nib.load(args.input_path)
     print(volume_img.shape)
     # pad volume to 16 slices
-    if volume_img.shape[2] < Z_DIM:
-        volume_data = np.zeros((volume_img.shape[0], volume_img.shape[1], Z_DIM, volume_img.shape[3]))
-        volume_data[:, :, :volume_img.shape[2], :] = volume_img.get_fdata()
+    # if volume_img.shape[2] < Z_DIM:
+    #     print('Padding volume to 16 slices')
+    #     volume_data = np.zeros((volume_img.shape[0], volume_img.shape[1], 16, volume_img.shape[3]))
+    #     volume_data[:, :, :volume_img.shape[2], :] = volume_img.get_fdata()
+    #     print(volume_data.shape)
 
     # if z dim isn't divisible by 2^3, crop to nearest divisible by 2^3
-    elif volume_img.shape[2] % 2**3 != 0:
-        volume_data = volume_img.get_fdata()[:, :, :volume_img.shape[2] - volume_img.shape[2] % 2**3, :]
-    else:
-        volume_data = np.array(volume_img.dataobj)
-    print(volume_data.shape)
+    # elif volume_img.shape[2] % 2**3 != 0:
+    #     volume_data = volume_img.get_fdata()[:, :, :volume_img.shape[2] - volume_img.shape[2] % 2**3, :]
+    # else:
+    #     volume_data = np.array(volume_img.dataobj)
+    volume_data = volume_img.get_fdata()
 
     print('Preprocessing')
     vol_pre = preprocessing(volume_data)
 
     print('Loading model')
-    model = unet3d(img_size = (volume_img.shape[0], volume_img.shape[1], None, T_DIM),\
+    model = unet3d(img_size = (X_DIM, Y_DIM, Z_DIM, T_DIM),\
                      learning_rate = 1e-3,\
                      learning_decay = 1e-9)
 
@@ -47,13 +49,12 @@ def inference_mode(args):
 
     print('Prediction')
     y_pred_mask, y_pred_vf, _ = model.predict(vol_pre)
-    # tf.print(y_pred_mask, output_stream=sys.stdout)
     # y_pred_mask = y_pred_mask > 0.8
     # y_pred_mask = y_pred_mask * 2.0
     y_pred_mask = y_pred_mask.astype(float)
 
     print('Resizing volume (padding)')
-    y_pred_mask_rz = resize_mask(y_pred_mask)#padding
+    y_pred_mask_rz = resize_mask(y_pred_mask, volume_data)#padding
 
     return  y_pred_vf, y_pred_mask_rz, volume_img
 
@@ -148,8 +149,8 @@ def evaluate_model(args):
             x = np.arange(len(y_pred_vf[0]))
             plt.yticks(fontsize=19)
             plt.xticks(fontsize=19)
-            plt.plot(x, y_pred_vf[0], 'r', label='Auto', lw=3)
-            plt.plot(x, (batch_label[1])[0], 'b', label='Manual', lw=3)
+            plt.plot(x, y_pred_vf[0] / y_pred_vf[0][0], 'r', label='Auto', lw=3)
+            plt.plot(x, (batch_label[1])[0] / (batch_label[1])[0][0], 'b', label='Manual', lw=3)
             plt.legend(loc="upper right", fontsize=16)
             plt.savefig(os.path.join(args.save_output_path, data[i]+'.png'), bbox_inches="tight")
             plt.close()
@@ -195,11 +196,24 @@ if __name__== "__main__":
                     # remove .nii from file
                     file = file[:-4]
                     nib.save(mask_img, args.save_output_path+ '/' + file + '_mask.nii')
-                    np.save(args.save_output_path+'/aif.npy', vf)
-                    np.save(args.save_output_path+'/mask.npy', mask)
-                    scipy.io.savemat(args.save_output_path+'/mask.mat',{'mask_pred':mask})
+                    # np.save(args.save_output_path+'/aif.npy', vf)
+                    # np.save(args.save_output_path+'/mask.npy', mask)
+                    # scipy.io.savemat(args.save_output_path+'/mask.mat',{'mask_pred':mask})
                     # remove rest of last file from input_path
                     args.input_path = args.input_path[:-len(file)-5]
+                    # plot vascular function
+                    if args.save_image == 1:
+                        plt.figure(figsize=(15,5), dpi=250)
+                        plt.subplot(1,2,1)
+                        plt.title('Vascular Function (VF):'+file)
+                        x = np.arange(len(vf[0]))
+                        plt.yticks(fontsize=19)
+                        plt.xticks(fontsize=19)
+                        plt.plot(x, vf[0] / vf[0][0], 'r', label='Auto', lw=3)
+                        plt.legend(loc="upper right", fontsize=16)
+                        plt.savefig(os.path.join(args.save_output_path, file+'.png'), bbox_inches="tight")
+                        plt.close()
+                        print('Saved image at:', args.save_output_path)
         else:
             vf, mask, bozo = inference_mode(args)
             mask = mask.squeeze()
@@ -209,6 +223,19 @@ if __name__== "__main__":
             # np.save(args.save_output_path+'/aif.npy', vf)
             # np.save(args.save_output_path+'/mask.npy', mask)
             # scipy.io.savemat(args.save_output_path+'/mask.mat',{'mask_pred':mask})
+            # plot vascular function
+            if args.save_image == 1:
+                plt.figure(figsize=(15,5), dpi=250)
+                plt.subplot(1,2,1)
+                plt.title('Vascular Function (VF): '+args.input_path)
+                x = np.arange(len(vf[0]))
+                plt.yticks(fontsize=19)
+                plt.xticks(fontsize=19)
+                plt.plot(x, vf[0] / vf[0][0], 'r', label='Auto', lw=3)
+                plt.legend(loc="upper right", fontsize=16)
+                plt.savefig(os.path.join(args.save_output_path+'/mask_vf.png'), bbox_inches="tight")
+                plt.close()
+                print('Vascular Function (VF) saved as image in: ', args.save_output_path+'/mask_vf.png')
     elif args.mode == "training":
         print('Mode:', args.mode)
         training_model(args)

model_vif.py
@@ -20,7 +20,10 @@ def loss_mae(y_true, y_pred, scale_loss = True):
     y_pred_f = flatten(y_pred / (y_pred[:, 0]))
     
     mae = tf.keras.losses.MeanAbsoluteError()
-    loss = mae(y_true_f, y_pred_f, sample_weight=[1, 1, 1, 1, 1, 1, 2])
+    # weight 6:2 ratio of first 10 repetitions to last 22 repetitions
+    weights = np.concatenate((np.ones(10)*6, np.ones(22)))
+    loss = mae(y_true_f, y_pred_f, sample_weight=weights)
+    
     if scale_loss:
         return 200*loss
     else:
@@ -57,6 +60,24 @@ def computeCurve(tensor):
 def computeQuality(tensor):
     mask = tensor[0]
     roi = tensor[1]
+    print(mask)
+    print(roi)
+
+    num = tf.keras.backend.sum(roi, axis=(1, 2, 3), keepdims=False)
+    den = tf.keras.backend.sum(mask, axis=(1, 2, 3), keepdims=False)
+    curve = tf.math.divide(num,den + 1e-8)
+    curve = tf.cast(curve, tf.float32)
+    print(curve)
+    max = tf.reduce_max(curve)
+    print(max)
+    max_base_ratio = max
+    max_end_ratio = tf.divide(max, curve[-1])
+    print(max_base_ratio)
+    print(max_end_ratio)
+    quality = 1/max_base_ratio + 1/max_end_ratio
+    print(quality)
+
+    return curve
     
 
 def normalizeOutput(tensor):
@@ -70,7 +91,6 @@ def getVolume(tensor):
     mask_thresholded = mask * tf.cast(threshold, dtype=tf.float32)
     
     vol = tf.math.count_nonzero(mask_thresholded)
-    # tf.print(vol, output_stream=sys.stdout, summarize = -1)
     return vol
 
 def loss_computeCofDistance3D(y_true, y_pred):
@@ -107,13 +127,13 @@ def loss_quality(y_true, y_pred):
     flatten = tf.keras.layers.Flatten()
     
     # normalize data to emphasize intensity curve shape over magnitudes
-    y_true_f = flatten(y_true / (y_true[:, 0]))
+    # y_true_f = flatten(y_true / (y_true[:, 0]))
     y_pred_f = flatten(y_pred / (y_pred[:, 0]))
     
-    max_base_ratio_true = max(y_true_f)
+    # max_base_ratio_true = max(y_true_f)
     max_base_ratio_pred = max(y_pred_f)
     
-    max_end_ratio_true = max(y_true_f) / y_true_f[-1]
+    # max_end_ratio_true = max(y_true_f) / y_true_f[-1]
     max_end_ratio_pred = max(y_pred_f) / y_pred_f[-1]
     
     loss = 1/max_base_ratio_pred + 1/max_end_ratio_pred
@@ -188,7 +208,7 @@ def unet3d(img_size = (None, None, None),learning_rate = 1e-8,\
     model.compile(optimizer=opt, loss={
         "lambda_normalization" : [loss_computeCofDistance3D],
         "lambda_vf" : [loss_mae],
-        "lambda_vol" : [loss_volume]
+        "lambda_vol" : [loss_volume],
         # "lambda_quality" : [loss_quality]
     }, loss_weights = weights)
 

utils_vif.py
@@ -19,12 +19,14 @@ def preprocessing(vol):
     #     vol = vol[:,:,:,(0, 4, 5, 6, 7, 8, -1)]
     
     # make image arrays of uniform size
-    batch_images = np.empty((1, vol.shape[0], vol.shape[1], vol.shape[2], T_DIM))
-    vol_crop = np.zeros([vol.shape[0], vol.shape[1], vol.shape[2], T_DIM])
+    batch_images = np.empty((1, X_DIM, Y_DIM, Z_DIM, T_DIM))
+    vol_crop = np.zeros([X_DIM, Y_DIM, Z_DIM, T_DIM])
     vol = (vol-np.min(vol))/((np.max(vol)-np.min(vol)))
     # vol_crop = vol[102:(256-34), 60:(240-60),:,:]
     # vol_crop = vol[57:281, 0:256,:,:]
-    vol_crop = vol
+    # vol_crop = vol
+    vol_crop = scipy.ndimage.zoom(vol, (X_DIM / vol.shape[0], Y_DIM / vol.shape[1], Z_DIM / vol.shape[2], T_DIM / vol.shape[3]), order=1)
+
     # plot the first slice of the first volume
     # plt.imshow(vol_crop[:,:,0,0])
     # plt.show()
@@ -34,15 +36,20 @@ def preprocessing(vol):
     return batch_images
 
 
-def resize_mask(mask):
+def resize_mask(mask, vol):
 
     # mask_rz = np.zeros([mask.shape[0], 256, 240, 120], dtype=float)
     # mask_rz[:,102:(256-34), 60:(240-60),:] = mask[:,:,:,:,0]
     # mask_rz = np.zeros([mask.shape[0], 320, 320, 16], dtype=float)
     # mask_rz[:,57:281, 0:296,:] = mask[:,:,:,:,0]
-    mask_rz = np.zeros([mask.shape[0], X_DIM, Y_DIM, Z_DIM], dtype=float)
-    mask_rz = mask[:,:,:,:,0]
-    
+    # mask_rz = np.zeros([mask.shape[0], X_DIM, Y_DIM, Z_DIM], dtype=float)
+    # mask_rz = mask[:,:,:,:,0]
+    # restore mask to original size
+    print("bozo: " + str(mask.shape))
+    mask_rz = scipy.ndimage.zoom(mask, (1, vol.shape[0] / X_DIM, vol.shape[1] / Y_DIM, vol.shape[2] / Z_DIM, 1), order=1)
+    print("ozob: " + str(mask_rz.shape))
+    # mask_rz = np.round(mask_rz)
+    # mask_rz = mask_rz.astype(int)
     return mask_rz
 
 def load_data(path):