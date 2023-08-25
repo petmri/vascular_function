eaff1f7af8e43606c633364ababb8cb06560f0d6
Lucas Saca <LLSaca@llu.edu>
2023-08-25 14:48:00 -0700

    update AIF aggregate graph with 95% conf interval + aligned peaks + differentiated datasets

--- sanity_check.py
+++ sanity_check.py
@@ -10,6 +10,7 @@ def plotAIFCurves(path):
     files = os.listdir(path + '/images/')
     print(files)
 
+    avg_curve = []
     # loop through all files
     for file in files:
         # if file.startswith("500"):
@@ -38,15 +39,37 @@ def plotAIFCurves(path):
         #     print(file + " has a weak AIF curve with " + str(intensities[1]) + " and " + str(intensities[2]))
         # if intensities[2] > intensities[1] or intensities[3] > intensities[2]+.5:
         #     print(file + " has a delayed injection with " + str(intensities[1]) + " and " + str(intensities[2]) + " and " + str(intensities[3]))
-        if any(intensities[10:30] < 2):
-            print(file + " has an intensity < 2")
+        # if any(intensities[10:30] < 2):
+        #     print(file + " has an intensity < 2")
+        # line up curve peaks
+        max_index = np.argmax(intensities)
+        intensities = np.roll(intensities, -max_index+3)
+        intensities = intensities[0:45]
+        if file.startswith("110"):
+            plt.plot(intensities, linewidth=0.1, color='blue', alpha=0.5)
+        elif file.startswith("500"):
+            plt.plot(intensities, linewidth=0.1, color='red', alpha=0.5)
+        elif file.startswith("Pat"):
+            plt.plot(intensities, linewidth=0.1, color='green', alpha=0.5)
+        else:
+            plt.plot(intensities, linewidth=0.1, color='gray', alpha=0.5)
 
-        plt.plot(intensities)
-    
+
+
+        avg_curve.append(intensities[0:45])
+    avg_curve = np.asarray(avg_curve)
+    avg_curve = np.mean(avg_curve, axis=0)
+    plt.plot(avg_curve, linewidth=1, color='black')
+    # plot 95% confidence interval
+    plt.fill_between(np.arange(0, len(avg_curve)), avg_curve - 1.96*np.std(avg_curve[0:50]), avg_curve[0:50] + 1.96*np.std(avg_curve), alpha=0.3)
+    plt.xlabel('Time (s)')
+    plt.ylabel('Normalized Intensity')
+    plt.title('AIF Curves')
+    # plt.legend(['AIF Curves', 'Average AIF Curve'])
     # plt.show()
 
     # save plot
-    plt.savefig(path + '/AIF_curves.png')
+    plt.savefig(path + '/AIF_curves.svg', bbox_inches='tight')
 
 if __name__== "__main__":
     # path to set (test/train/val)
