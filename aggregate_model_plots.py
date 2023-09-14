905fb5c999eea5e526b32c1be1fa04521ae3fe20
Lucas Saca <LLSaca@llu.edu>
2023-09-14 15:08:12 -0700

    make ROI mosaics - mosaic of models for subject & mosaic of that mosaic. Mosaic-ception.

--- aggregate_model_plots.py
+++ aggregate_model_plots.py
@@ -8,7 +8,9 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import tensorrt
 import nibabel as nib
+from matplotlib import colors as mcolors
 from PIL import Image
+from scipy import ndimage
 from tensorflow import keras
 from tensorflow.keras import layers
 
@@ -16,8 +18,10 @@ from model_vif import *
 from utils_vif import *
 
 # List of model weight paths
-model_paths = ['/home/mrispec/AUTOAIF_DATA/weights/run2_fullMAE/model_weight.h5', '/home/mrispec/AUTOAIF_DATA/weights/3MAEweights/model_weight.h5', '/home/mrispec/AUTOAIF_DATA/weights/gaggar_weights/model_weight.h5',
-               '/home/mrispec/AUTOAIF_DATA/weights/gaggar_9-1/model_weight.h5', '/home/mrispec/AUTOAIF_DATA/weights/gaggar_9-8_cos/model_weight.h5']
+# model_paths = ['/home/mrispec/AUTOAIF_DATA/weights/run2_fullMAE/model_weight.h5', '/home/mrispec/AUTOAIF_DATA/weights/gaggar_weights/model_weight.h5', '/home/mrispec/AUTOAIF_DATA/weights/gaggar_9-1/model_weight.h5',
+#                '/home/mrispec/AUTOAIF_DATA/weights/gaggar_9-8_cos/model_weight.h5', '/home/mrispec/AUTOAIF_DATA/weights/gaggar_9-9_maedice/model_weight.h5', '/home/mrispec/AUTOAIF_DATA/weights/gaggar_9-11_mse/model_weight.h5', 
+#                '/home/mrispec/AUTOAIF_DATA/weights/gaggar_9-11_huber/model_weight.h5']
+model_paths = ['/home/mrispec/AUTOAIF_DATA/weights/run2_fullMAE/model_weight.h5', '/home/mrispec/AUTOAIF_DATA/weights/gaggar_9-11_mse/model_weight.h5','/home/mrispec/AUTOAIF_DATA/weights/gaggar_9-11_huber/model_weight.h5']
 
 # strip the model weight paths to get the model names
 model_names = [os.path.basename(path[:-16]) for path in model_paths]
@@ -35,6 +39,8 @@ def process_image(image_path):
     vol_pre = preprocessing(volume_data)
 
     vfs = []
+    masks = []
+    peak_index = []
     for i, model_weight in enumerate(model_paths):
         model = unet3d(img_size = (X_DIM, Y_DIM, Z_DIM, T_DIM),\
                         learning_rate = 1e-3,\
@@ -61,11 +67,14 @@ def process_image(image_path):
         vf = num / (den + 1e-8)
         # vf = y_pred_vf
         vfs.append(vf)
-    
-    # Create subplot of prediction graphs
+        masks.append(mask_thresholded)
+        peak_index.append(np.argmax(vf))
+
+    peak_index = max(set(peak_index), key=peak_index.count)
+    # plot prediction
     x = np.arange(len(vfs[0][0]))
     plt.figure(figsize=(15,7), dpi=100)
-    colors = ['r', 'g', 'c', 'm', 'y', 'k']
+    colors = ['r', 'g', 'c', 'm', 'y', 'k', 'tab:orange']
     for i, vf in enumerate(vfs):
         plt.title('Vascular Function (VF): ' + image_path)
         # set axis titles
@@ -80,7 +89,7 @@ def process_image(image_path):
     mask_dir = mask_dir + '/masks'
     file = image_path.split('/')[-1].split('.')[0]
     path = image_path[:-len(image_path.split('/')[-1])-1]
-    # plot manual mask if it exists
+    # plot manual curve if it exists
     if os.path.isfile(mask_dir + '/' + file + '.nii') or os.path.isfile(path + '/aif.nii'):
         if os.path.isfile(mask_dir + '/' + file + '.nii'):
             img = nib.load(mask_dir + '/' + file + '.nii')
@@ -109,6 +118,51 @@ def process_image(image_path):
     plt.savefig(os.path.join(output_folder, file + '_curve.png'), bbox_inches="tight")
     plt.close()
 
+    # overlay mask on image
+    z_rois = []
+    try:
+        img = nib.load(path + '/' + file + '.nii')
+    except:
+        img = nib.load(path + '/' + file + '.nii.gz')
+    img_data = img.get_fdata()
+    img_data = img_data.squeeze()
+    img_data = np.rot90(img_data, k=1, axes=(0,1))
+    for i, mask in enumerate(masks):
+        # find center of mass of mask
+        CoM = ndimage.center_of_mass(mask)
+        # round to nearest integer
+        z_roi = np.round(CoM[2]).astype(int)
+        if z_roi > img_data.shape[2]:
+            z_roi = img_data.shape[2] - 1
+        elif z_roi < 0:
+            z_roi = 0
+        z_rois.append(z_roi)
+        # rotate mask 90 degrees counter-clockwise
+        mask = np.rot90(mask, k=1, axes=(0,1))
+        # overlay mask, values below 0.5 are transparent
+        # cmap = mcolors.LinearSegmentedColormap.from_list('custom cmap', [(0, 0, 0, 0), 'blue', 'green', 'red'])
+        # cmap = cmaps[i % 3]
+        # plot mask from each model
+        plt.figure(figsize=(15,5), dpi=125)
+        plt.title(file + ' ' + model_names[i])
+        # text top left saying % of voxels in slice
+        plt.text(5, 15, str(np.round(np.sum(mask[:,:,z_roi]) / (np.round(np.sum(mask))) * 100, 2)) + '% of ' + str(np.round(np.sum(mask))) + ' voxels in slice ' + str(z_roi), fontsize=14, color='white')
+        plt.axis('off')
+        cmap = mcolors.LinearSegmentedColormap.from_list('custom cmap', [(0, 0, 0, 0), 'blue', 'red', 'green'])
+        if file.startswith('5'):
+            plt.imshow(img_data[:,:,z_roi,peak_index], cmap='gray', vmax=500)
+        else:
+            plt.imshow(img_data[:,:,z_roi,peak_index], cmap='gray')
+        plt.imshow(mask[:,:,z_roi], cmap=cmap, alpha=0.5)
+        plt.savefig(os.path.join(output_folder, file + '_' + model_names[i] + '_mask.png'), bbox_inches="tight")
+        plt.close()
+    # get mode of z_rois
+    z_roi_mode = max(set(z_rois), key=z_rois.count)
+    # for i, z_roi in enumerate(z_rois):
+    print('Saved masked image at:', output_folder, 'Showing slice:', z_roi_mode)
+    # plt.savefig(os.path.join(output_folder, file + '_mask.png'), bbox_inches="tight")
+    # plt.close()
+
 
 for image in os.listdir(image_folder):
     image_path = os.path.join(image_folder, image)
@@ -118,22 +172,22 @@ for image in os.listdir(image_folder):
 results_folder = output_folder
 
 # Output path for the final mosaic image
-final_mosaic_path = results_folder + "/final_mosaic.png"
+curve_mosaic_path = results_folder + "/curve_mosaic.png"
 
 # Get a list of all result image file paths
-result_image_paths = [os.path.join(results_folder, filename) for filename in os.listdir(results_folder) if filename.endswith(".png")]
+result_curve_paths = [os.path.join(results_folder, filename) for filename in os.listdir(results_folder) if filename.endswith("curve.png")]
 
 # Determine the number of images per row in the mosaic
 images_per_row = 5  # You can adjust this based on your preference
 
-# Open all result images and calculate dimensions for the final mosaic
-result_images = [Image.open(image_path) for image_path in result_image_paths]
+# Open all result curve images and calculate dimensions for the final mosaic
+result_images = [Image.open(image_path) for image_path in result_curve_paths]
 image_width, image_height = result_images[0].size
 mosaic_width = image_width * images_per_row
 mosaic_height = image_height * ((len(result_images) - 1) // images_per_row + 1)
 
 # Create a new blank mosaic image
-final_mosaic = Image.new("RGB", (mosaic_width, mosaic_height), (255, 255, 255))
+curve_mosaic = Image.new("RGB", (mosaic_width, mosaic_height), (255, 255, 255))
 
 # Paste each result image onto the mosaic
 for i, result_image in enumerate(result_images):
@@ -141,9 +195,79 @@ for i, result_image in enumerate(result_images):
     col = i % images_per_row
     x_offset = col * image_width
     y_offset = row * image_height
-    final_mosaic.paste(result_image, (x_offset, y_offset))
+    curve_mosaic.paste(result_image, (x_offset, y_offset))
 
 # Save the final mosaic image
-final_mosaic.save(final_mosaic_path)
+curve_mosaic.save(curve_mosaic_path)
 
-print("Final mosaic image created:", final_mosaic_path)
+print("Curve mosaic image created:", curve_mosaic_path)
+
+# Now do the same for the masked images, but one subject at a time
+# get subject names
+subjects = [os.path.basename(path) for path in os.listdir(image_folder)]
+subjects = list(set([subject[:-4] for subject in subjects]))
+print(subjects)
+
+for subject in subjects:
+    # Output path for the mask mosaics
+    mosaic_path = results_folder + "/" + subject + "_mosaic.png"
+
+    # Get a list of all result image file paths
+    result_paths = [os.path.join(results_folder, filename) for filename in os.listdir(results_folder) if (filename.startswith(subject) and filename.endswith("mask.png"))]
+
+    # Determine the number of images per row in the mosaic
+    images_per_row = len(model_names)  # You can adjust this based on your preference
+
+    # Open all result images and calculate dimensions for the final mosaic
+    result_images = [Image.open(image_path) for image_path in result_paths]
+    image_width, image_height = result_images[0].size
+    mosaic_width = image_width * images_per_row
+    mosaic_height = image_height * ((len(result_images) - 1) // images_per_row + 1)
+
+    # Create a new blank mosaic image
+    mosaic = Image.new("RGB", (mosaic_width, mosaic_height), (255, 255, 255))
+
+    # Paste each result image onto the mosaic
+    for i, result_image in enumerate(result_images):
+        row = i // images_per_row
+        col = i % images_per_row
+        x_offset = col * image_width
+        y_offset = row * image_height
+        mosaic.paste(result_image, (x_offset, y_offset))
+
+    # Save the final mosaic image
+    mosaic.save(mosaic_path)
+
+    print("Mosaic image created:", mosaic_path)
+
+# I guess we can unify the mask mosaics
+# Output path for the final mosaic image
+mosaic_path = results_folder + "/mask_mosaic.png"
+
+# Get a list of all result image file paths
+result_paths = [os.path.join(results_folder, filename) for filename in os.listdir(results_folder) if not filename.startswith('curve') and filename.endswith("_mosaic.png")]
+
+# Determine the number of images per row in the mosaic
+images_per_row = 3  # You can adjust this based on your preference
+
+# Open all result images and calculate dimensions for the final mosaic
+result_images = [Image.open(image_path) for image_path in result_paths]
+image_width, image_height = result_images[0].size
+mosaic_width = image_width * images_per_row
+mosaic_height = image_height * ((len(result_images) - 1) // images_per_row + 1)
+
+# Create a new blank mosaic image
+mosaic = Image.new("RGB", (mosaic_width, mosaic_height), (255, 255, 255))
+
+# Paste each result image onto the mosaic
+for i, result_image in enumerate(result_images):
+    row = i // images_per_row
+    col = i % images_per_row
+    x_offset = col * image_width
+    y_offset = row * image_height
+    mosaic.paste(result_image, (x_offset, y_offset))
+
+# Save the final mosaic image
+mosaic.save(mosaic_path)
+
+print("Mask giga mosaic image created:", mosaic_path)
